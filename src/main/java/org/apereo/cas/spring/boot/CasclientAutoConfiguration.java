613d485d919fb1ef553fec2dd5271ac0b28ac7ef
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2018-04-02 11:29:26 +0800

    调整

--- src/main/java/org/apereo/cas/spring/boot/CasclientAutoConfiguration.java
+++ src/main/java/org/apereo/cas/spring/boot/CasclientAutoConfiguration.java
@@ -13,6 +13,7 @@ import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
 import org.jasig.cas.client.util.AssertionThreadLocalFilter;
 import org.jasig.cas.client.util.ErrorRedirectFilter;
 import org.jasig.cas.client.util.HttpServletRequestWrapperFilter;
+import org.jasig.cas.client.validation.AbstractTicketValidationFilter;
 import org.jasig.cas.client.validation.Cas10TicketValidationFilter;
 import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter;
 import org.jasig.cas.client.validation.Cas30ProxyReceivingTicketValidationFilter;
@@ -73,9 +74,9 @@ public class CasclientAutoConfiguration {
 	 * CAS Error Redirect Filter </br>
 	 */
 	@Bean
-	public FilterRegistrationBean errorRedirectFilter() {
+	public FilterRegistrationBean<ErrorRedirectFilter> errorRedirectFilter() {
 		
-		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+		FilterRegistrationBean<ErrorRedirectFilter> filterRegistration = new FilterRegistrationBean<ErrorRedirectFilter>();
 		filterRegistration.setFilter(new ErrorRedirectFilter());
 		filterRegistration.setEnabled(casProperties.isErrorRedirect());
 		
@@ -99,8 +100,8 @@ public class CasclientAutoConfiguration {
 	 * 该过滤器用于实现单点登出功能，单点退出配置，一定要放在其他filter之前
 	 */
 	@Bean
-	public FilterRegistrationBean singleSignOutFilter() {
-		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+	public FilterRegistrationBean<SingleSignOutFilter> singleSignOutFilter() {
+		FilterRegistrationBean<SingleSignOutFilter> filterRegistration = new FilterRegistrationBean<SingleSignOutFilter>();
 		filterRegistration.setFilter(new SingleSignOutFilter());
 		filterRegistration.setEnabled(casProperties.isEnabled());
 		
@@ -127,8 +128,8 @@ public class CasclientAutoConfiguration {
 	 * 该过滤器负责对Ticket的校验工作
 	 */
 	@Bean
-	public FilterRegistrationBean ticketValidationFilter() {
-		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+	public FilterRegistrationBean<AbstractTicketValidationFilter> ticketValidationFilter() {
+		FilterRegistrationBean<AbstractTicketValidationFilter> filterRegistration = new FilterRegistrationBean<AbstractTicketValidationFilter>();
 		filterRegistration.setEnabled(casProperties.isEnabled()); 
 		if(Protocol.CAS1.equals(casProperties.getProtocol())) {
 			filterRegistration.setFilter(new Cas10TicketValidationFilter());
@@ -236,8 +237,8 @@ public class CasclientAutoConfiguration {
 	 * 该过滤器负责用户的认证工作
 	 */
 	@Bean
-	public FilterRegistrationBean authenticationFilter() {
-		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+	public FilterRegistrationBean<AuthenticationFilter> authenticationFilter() {
+		FilterRegistrationBean<AuthenticationFilter> filterRegistration = new FilterRegistrationBean<AuthenticationFilter>();
 		if (Protocol.SAML11.equals(casProperties.getProtocol())) {
 			filterRegistration.setFilter(new Saml11AuthenticationFilter());
 		} else {
@@ -274,8 +275,8 @@ public class CasclientAutoConfiguration {
 	 * 该过滤器对HttpServletRequest请求包装， 可通过HttpServletRequest的getRemoteUser()方法获得登录用户的登录名
 	 */
 	@Bean
-	public FilterRegistrationBean requestWrapperFilter() {
-		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+	public FilterRegistrationBean<HttpServletRequestWrapperFilter> requestWrapperFilter() {
+		FilterRegistrationBean<HttpServletRequestWrapperFilter> filterRegistration = new FilterRegistrationBean<HttpServletRequestWrapperFilter>();
 		filterRegistration.setFilter(new HttpServletRequestWrapperFilter());
 		filterRegistration.setEnabled(casProperties.isEnabled()); 
 		filterRegistration.addInitParameter(ConfigurationKeys.IGNORE_CASE.getName(), String.valueOf(casProperties.isIgnoreCase()));
@@ -294,8 +295,8 @@ public class CasclientAutoConfiguration {
 	 * 这个类把Assertion信息放在ThreadLocal变量中，这样应用程序不在web层也能够获取到当前登录信息
 	 */
 	@Bean
-	public FilterRegistrationBean assertionThreadLocalFilter() {
-		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+	public FilterRegistrationBean<AssertionThreadLocalFilter> assertionThreadLocalFilter() {
+		FilterRegistrationBean<AssertionThreadLocalFilter> filterRegistration = new FilterRegistrationBean<AssertionThreadLocalFilter>();
 		filterRegistration.setFilter(new AssertionThreadLocalFilter());
 		filterRegistration.setEnabled(casProperties.isEnabled());
 		filterRegistration.addUrlPatterns(casProperties.getAssertionThreadLocalFilterUrlPatterns());
